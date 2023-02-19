9de88b2ec069d49b05620ed4a875fe6a08eb7352
Sindhu S <sindhu@bytebeam.io>
2023-02-20 02:04:45 +0530

    Turn off signing for clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/sindhubb/clerical"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]])
