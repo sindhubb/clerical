bb527f1fd0db788378568f43ebd261b73d8d3fd0
Sindhu S <sindhu@bytebeam.io>
2023-02-20 02:01:13 +0530

    Change to clojars group-id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bytebeam/lein-template.clerical "0.1.0"
+(defproject org.clojars.sindhusbb/lein-template.clerical "0.1.0"
   :description "Clerk + your cool project!"
   :url "https://github.com/sindhubb/clerical"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
